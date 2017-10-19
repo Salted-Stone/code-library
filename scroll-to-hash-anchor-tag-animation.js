26aab6a5c4b3c6397a8c7ec23565f507da388402
Alex <alex.sivro@saltedstone.com>
2017-10-19 13:14:38 -0700

    scroll to hash update
    added alternate script and changed formatting

--- scroll-to-hash-anchor-tag-animation.js
+++ scroll-to-hash-anchor-tag-animation.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-  $('a[href*=#]').on('click', function(event){
-      event.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-  });
+
+	// Script 1
+	$('a[href*=#]').on('click', function(event){
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	});
+
+	// Script 2 - A Little bigger but more comprehensive
+	$(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
 });
