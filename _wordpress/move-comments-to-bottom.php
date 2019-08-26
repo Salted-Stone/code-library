860acf91788b7d6d9229c077aa67d2ed8369b92d
Alex <alex.sivro@saltedstone.com>
2019-08-25 20:48:40 -0700

    Created move comments to bottom
    wordpress move comments to bottom

---
+++ _wordpress/move-comments-to-bottom.php
@@ -0,0 +1,8 @@
+function s2_move_comment_field_to_bottom( $fields ) {
+	$comment_field = $fields['comment'];
+	unset( $fields['comment'] );
+	$fields['comment'] = $comment_field;
+	return $fields;
+}
+
+add_filter( 'comment_form_fields', 's2_move_comment_field_to_bottom' );
